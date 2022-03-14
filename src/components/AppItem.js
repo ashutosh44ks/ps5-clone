5779ae4b5eb9d4818c51a9ac2e6b5b9ce48161e7
Ashutosh Kumar Singh <71124485+ashutosh44ks@users.noreply.github.com>
2022-03-14 13:16:36 +0530

    update AppItem hover text css

--- src/components/AppItem.js
+++ src/components/AppItem.js
@@ -4,7 +4,7 @@ const AppItem = ({ item }) => {
   return (
     <div>
       <div className="app-item bright-text">{item.icon}</div>
-      <div className="app-item-hover center-text dull-text">{item.name}</div>
+      <div className="app-item-hover dull-text">{item.name}</div>
     </div>
   );
 };
