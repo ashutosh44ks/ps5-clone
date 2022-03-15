ca2f4c9b3484f2ff7254bd9d0d1256c8ed91999a
Ashutosh Kumar Singh <71124485+ashutosh44ks@users.noreply.github.com>
2022-03-15 13:15:26 +0530

    fixed bug where card was overflowing it's container

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,22 +1,28 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./stylesheets/ResultCard.css";
 
 const ResultCard = ({ item }) => {
+  let navigate = useNavigate();
+  useEffect(() => {
+    const card = document.querySelector(".card-container");
+    card.addEventListener("click", () =>
+      navigate("/ps5-clone/AppPage", item.Title)
+    );
+  });
   return (
-    <Link to="/ps5-clone/AppPage" state={item.Title}>
-      <div
-        className="card-container"
-        style={{ background: `url(${item.Poster})` }}
-      >
-        <div className="card-body">
-          <div className="card-title">{item.Title}</div>
-          <div className="card-info">
-            <div className="card-price">{item.Year}</div>
-            <div className="card-type">{item.Type}</div>
-          </div>
+    <div
+      className="card-container"
+      style={{ background: `url(${item.Poster})` }}
+    >
+      <div className="card-body">
+        <div className="card-title">{item.Title}</div>
+        <div className="card-info">
+          <div className="card-price">{item.Year}</div>
+          <div className="card-type">{item.Type}</div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
